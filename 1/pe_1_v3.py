07e161823fafb2a312a99e8bc756aea1c14b91cb
a-hawkins <36182040+a-hawkins@users.noreply.github.com>
2020-05-14 15:50:46 -0500

    Removed forgotten debugging print statement

--- 1/pe_1_v3.py
+++ 1/pe_1_v3.py
@@ -5,7 +5,6 @@ target = 999
 
 def divisibleSum(multiple, target):
 	n = int(target/multiple)
-	print (int(multiple * (n*(n+1))/2))
 	return int(multiple * (n*(n+1))/2)
 
 print(divisibleSum(3, target) + divisibleSum(5, target) - divisibleSum(15, target))
