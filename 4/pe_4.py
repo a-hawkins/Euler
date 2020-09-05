1758c57ce8be2d8af2abf3f6279880db48d83845
a-hawkins <36182040+a-hawkins@users.noreply.github.com>
2020-09-05 00:34:18 -0500

    Added problem description and solution code for 4

---
+++ 4/pe_4.py
@@ -0,0 +1,21 @@
+def isPalindrome(n):
+    n = str(n)
+    if(len(n) == 1 or (len(n)==2 and n[0]==n[1])):
+        return True
+    if (n[0] != n[-1]):
+        return False
+    return isPalindrome(n[1: -1])
+
+n = m = 999
+biggestPal = 1
+prod = n*m
+while(n > 0 and m > 0 and prod > 10000):
+    #print(n, "+", m, "=", prod)
+    if(prod > biggestPal and isPalindrome(prod)):
+        biggestPal = prod
+    elif (prod > biggestPal):
+        m = m - 1
+    else:
+        m = n = n-1
+    prod = n * m
+print(biggestPal)
