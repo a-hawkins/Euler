5089d895ec95355812964a7990155c22e7999c29
a-hawkins <36182040+a-hawkins@users.noreply.github.com>
2020-09-05 00:33:34 -0500

    Added solution code for 3

---
+++ 3/pe_3.py
@@ -0,0 +1,17 @@
+def largestPrime(n):
+    largest = 1
+    i = 2
+    if(n%i==0):
+        largest = i
+        while(n%i==0):
+            n = n/i
+    i = 3
+    while(i <= n):
+        if(n%i==0):
+            largest = i
+            while(n%i==0):
+                n = n/i
+        i = i + 2
+    return largest
+
+print(largestPrime(600851475143))
