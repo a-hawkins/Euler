193aec4e42d7320bd647049ae0477ae7e1674698
a-hawkins <36182040+a-hawkins@users.noreply.github.com>
2020-09-06 01:10:05 -0500

    Added problem description and solution code for 7

---
+++ 7/pe_7_v1.py
@@ -0,0 +1,29 @@
+# PE 7, v1
+# 9-6-2020
+
+def sieve(n):
+    if(n < 2):
+        return [False]
+    primes = [True]*(n+1)
+    primes[0] = primes[1] = False
+    for i in range(2, n):
+        if(primes[i]):
+            for j in range(i**2, n, i):
+                primes[j] = False
+    return primes
+
+def nthPrime(n):
+    totalPrimes = 0
+    threshold = n * 2
+    primes = [False]
+    while (totalPrimes <= n):
+        totalPrimes = 0
+        primes = sieve(threshold)
+        for i in range(2, threshold):
+            if(primes[i]):
+                totalPrimes = totalPrimes + 1
+                if(totalPrimes == n):
+                    return i
+        threshold = threshold + n
+    return -1
+print(nthPrime(10001))
