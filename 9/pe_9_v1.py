921076d4bf5cc76d92b565de6b3631b8d5d0d3e1
a-hawkins <36182040+a-hawkins@users.noreply.github.com>
2020-09-06 16:16:03 -0500

    Added problem description and solution code for 9

---
+++ 9/pe_9_v1.py
@@ -0,0 +1,48 @@
+# PE 8 v1
+# 9-6-2020
+
+def generateTriple(m, n):
+# Generates a pythagorean triple fot m > n > 0
+    return [(m**2 - n**2), (2*n*m), (m**2 + n**2)]
+
+
+def isCoprime(a, b):
+    return (gcd(a,b) == 1)
+
+def gcd(a, b):
+    if a < b:
+        temp = a
+        a = b
+        b = temp
+
+    r = a % b
+
+    while(r > 0):
+        a = b
+        b = r
+        r = a % b
+    return b
+
+def specialTriple(n):
+    i = 0
+    primativeTriple = triple = [0, 0, 0]
+    while (sum(triple) != n):
+        j = 1
+        i  = i + 1
+        primativeTriple = triple = generateTriple(i, j)
+        while(j < i and sum(triple) < n):
+            if ((i % 2 == 0 or j % 1 == 0) and not isCoprime(i, j)):
+                k = 1
+                while(sum(triple) < n):
+                    k = k + 1
+                    triple = [x*k for x in primativeTriple]
+            j = j + 1
+    return triple
+
+triple = specialTriple(1000)
+print(triple[0]*triple[1]*triple[2])
+    
+
+
+
+    
