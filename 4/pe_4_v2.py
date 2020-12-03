b0a97d0f0bc50dba33e2c86b839c615f21570e6f
a-hawkins <36182040+a-hawkins@users.noreply.github.com>
2020-12-03 16:38:34 -0600

    Created v2 which trims around 1000 cases from being tested

---
+++ 4/pe_4_v2.py
@@ -0,0 +1,26 @@
+# PE 4, v2
+# 9-4-2020
+
+def isPalindrome(n):
+# Recursively checks if a string n is a palindrome and returns a boolean
+    n = str(n)
+    if(len(n) == 1 or (len(n)==2 and n[0]==n[1])):
+        return True
+    if (n[0] != n[-1]):
+        return False
+    return isPalindrome(n[1: -1])
+
+n = m = 999
+biggestPal = 1
+prod = n*m
+
+while(n > 0 and m > 0 and (n * n) >biggestPal):
+    count = count + 1
+    if(prod > biggestPal and isPalindrome(prod)):
+        biggestPal = prod
+    elif (prod > biggestPal):
+        m = m - 1
+    else:
+        m = n = n-1
+    prod = n * m
+print(biggestPal)
